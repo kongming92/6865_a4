36447be08a84fc7827ea5e9b358eeceb325f609e
Charles Liu <cliu2014@mit.edu>
2013-10-02 20:21:29 -0400

    Finish things

--- README.txt
+++ README.txt
@@ -1,26 +1,26 @@
-Name:
-MIT Email:
+Name: Charles Liu
+MIT Email: cliu2014@mit.edu
 
 Q1:How long did the assignment take?:
-{A1: your answer}
+{A1: 8 hours}
 
 Q2:Potential issues with your solution and explanation of partial completion (for partial credit):
-{A2: your answer}
+{A2: I couldn't tell whether my log SNR output is correct although it resembles the ones on the lecture slides. Everything else gives reasonable output.}
 
 Q3:Any extra credit you may have implemented:
-{A3: your answer}
+{A3: None}
 
 Q4:Collaboration acknowledgement (but again, you must write your own code):
-{A4: your answer}
+{A4: Ryan Lacey}
 
 Q5:What was most unclear/difficult?:
-{A5: your answer}
+{A5: It was difficult to test some things like log SNR without any examples. Also the offests for Green did not match the same for the others.}
 
 Q6:What was most exciting?:
-{A6: your answer}
+{A6: The things working}
 
 Q7:What ISO has better SNR?:
-{A7: your answer}
+{A7: ISO3200. Likely because the 400 image is underexposed, so less signal in the dark areas. With no scale factor (scale=1), log(SNR_400) = 1.233, log(SNR_3200) = 1.711 when averaged over all channels and pixels.}
 
 Q8:Which direction you decided to interpolate along for the edgeBasedGreenChannel?:
-{A8: your answer}
+{A8: Both, depending on the situation. At each pixel, calculate whether vertical or horizontal difference is smaller and go that way.}
--- a4Script.py
+++ a4Script.py
@@ -16,7 +16,6 @@ def getPNGsInDir(path):
     fnames = glob.glob(path+"*.png")
     pngs = list()
     for f in fnames:
-        #print f
         imi = io.getImage(f)
         pngs.append(imi)
     return pngs
@@ -25,9 +24,7 @@ def getRawPNGsInDir(path):
     fnames = glob.glob(path+"*.png")
     pngs = list()
     pngnames = list()
-    print path
     for f in fnames:
-        print f
         imi = io.imreadGrey(f)
         pngs.append(imi)
         pngnames.append(f)
@@ -43,7 +40,7 @@ def testAlign():
     align2 = np.zeros([30, 30, 3])
     align1[20, 20] = 1
     align2[22, 23] = 1
-    print 'We recommend creating a simple test case for testAlign() here. Meybe set one pixel of each image to be 1?'
+    # We recommend creating a simple test case for testAlign() here. Meybe set one pixel of each image to be 1?
     yalign, xalign = a4.align(align1, align2, 5)
     yalign2, xalign2 = a4.align(align2, align1, 5)
     print "alignment 1->2 is:"+'[{},{}]'.format(yalign, xalign)
@@ -63,30 +60,32 @@ def testAlignAndDenoise(imageList, outputname):
     imADN = a4.alignAndDenoise(imageList)
     io.imwrite(imADN, str(outputname+'_ADN_x%03d'%(len(imageList)))+".png")
 
-def testBasicDemosaic(raw, outputname, gos=1, rosy=1,rosx=1,bosy=0,bosx=0):
+def testBasicDemosaic(raw, outputname, gos=0, rosy=1,rosx=1,bosy=0,bosx=0):
     rout = a4.basicDemosaic(raw, gos, rosy, rosx, bosy, bosx)
     io.imwrite(rout, outputname+'_basicDemosaic.png')
 
-def testEdgeBasedGreenDemosaic(raw, outputname, gos =1, rosy=1,rosx=1,bosy=0,bosx=0):
+def testEdgeBasedGreenDemosaic(raw, outputname, gos=0, rosy=1,rosx=1,bosy=0,bosx=0):
     rout = a4.edgeBasedGreenDemosaic(raw, gos, rosy, rosx, bosy, bosx)
     io.imwrite(rout, outputname+'_edgeBasedGreenDemosaic.png')
 
-def testImprovedDemosaic(raw, outputname, gos =1, rosy=1,rosx=1,bosy=0,bosx=0):
+def testImprovedDemosaic(raw, outputname, gos=0, rosy=1,rosx=1,bosy=0,bosx=0):
     rout = a4.improvedDemosaic(raw, gos, rosy, rosx, bosy, bosx)
     io.imwrite(rout, outputname+'_improvedDemosaic.png')
 
 def testSergei():
-    sergeis, sergeiNames = getRawPNGsInDir("SergeiThird/")
+    sergeis, sergeiNames = getRawPNGsInDir("data/Sergei/")
     scount = 0
     for f in sergeis:
-        io.imwrite(a4.creatergb(f), str('Sergei'+'%03d'%(scount))+'.png')
+        io.imwrite(a4.sergeiRGB(f), str('Sergei'+'%03d'%(scount))+'.png')
         scount = scount +1
 
+def computeSNR(imList):
+   return a4.logSNR(imList, 1.0)
 
 #Input data:
 
-iso400 = getPNGsInDir("data/aligned-ISO400-16/")
-iso3200 = getPNGsInDir("data/aligned-ISO3200-16/")
+iso400 = getPNGsInDir("data/aligned-ISO400/")
+iso3200 = getPNGsInDir("data/aligned-ISO3200/")
 green = getPNGsInDir("data/green/")
 # raw, rawnames = getRawPNGsInDir("data/raw/")
 signsm = io.imreadGrey("data/raw/signs-small.png")
@@ -98,11 +97,13 @@ testSNR(iso400, "iso400")
 testSNR(iso3200, "iso3200")
 
 testAlign()
-testDenoise(green, 'green')
 testAlignAndDenoise(green, 'green')
 
 testBasicDemosaic(signsm, 'signSmall')
 testEdgeBasedGreenDemosaic(signsm, 'signSmall')
 testImprovedDemosaic(signsm, 'signSmall')
 
-# testSergei()
+testSergei()
+
+print "log(SNR) for ISO400", np.average(computeSNR(iso400))
+print "log(SNR) for ISO1600", np.average(computeSNR(iso3200))
